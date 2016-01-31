b5828149b99537aa4341d9a6d033a672f0828638
reneeb <info@perl-services.de>
2016-01-31 23:31:10 +0100

    update copyright date

--- Kernel/Output/HTML/OutputFilterOwnerChangeTicketView.pm
+++ Kernel/Output/HTML/OutputFilterOwnerChangeTicketView.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Output/HTML/OutputFilterOwnerChangeTicketView.pm
-# Copyright (C) 2015 Perl-Services.de, http://www.perl-services.de/
+# Copyright (C) 2015 - 2016 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
